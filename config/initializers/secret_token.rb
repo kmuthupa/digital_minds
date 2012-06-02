aafb72d2d82b9be34bcb9a3f8820c482c91553a2
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-06-02 16:43:58 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalMinds::Application.config.secret_token = '301e445048b973cafd0c5bafa5ee0498e859c01ad175d055a7a598437c5f60804ce03370b0ad31208fc1a723baf05b1aca497d854cf8ed9d7194e46e0b313ba5'
