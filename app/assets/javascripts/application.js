fdac0461b5f727999799e7cbf371fe871d968c34
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-06-10 07:26:30 +0530

    start contact page implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,77 @@ $(document).ready(function() {
 		interval: 3500
 	});
 	
+	/*** Contact modal ***/    
+    $('#contact').on('show', function () {
+        resetContactForm(true);
+    })
+
+    $("#contact-submit").click(function(e){
+        $('#contact').find('.inline-errors').remove();
+        var valid = true;
+        var url = $('#contact').attr('url');
+        var name = $('#contact').find('#name');
+        var email = $('#contact').find('#email');
+        var about = $('#contact').find('#about').val();
+        var comments = $('#contact').find('#comments');
+        if (name.val() === '' || name.val() === undefined) {
+            valid = false;
+            name.after("<p class='inline-errors'>can't be blank</p>");
+        }
+        if (email.val() === '' || email.val() === undefined || !isValidEmail(email.val())) {
+            valid = false;
+            email.after("<p class='inline-errors'>can't be blank or invalid</p>");
+        }
+        if (comments.val() === '' || comments.val() === undefined) {
+            valid = false;
+            comments.after("<p class='inline-errors'>can't be blank</p>");
+        }
+        if (valid) {
+            $("#contact-submit").hide();
+            $('#contact').find('.loader').show();
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: {
+                    name: name.val(),
+                    email: email.val(),
+                    about: about,
+                    comments: comments.val()
+                },
+                success: function(data) {
+                    $('#contact').modal('hide');
+                    if (data === 'success') {
+                        $('.notice-area').html("<div class='alert alert-success'>Thanks! Your message was successfully sent.</div>")
+                    }
+                    else {
+                        $('.notice-area').html("<div class='alert alert-error'>There was a problem. Please retry later.</div>")
+                    }
+                    setTimeout(hideFlashMessages, 3500);
+                }
+            });
+        }
+    });
+
+	var resetContactForm = function(toggle) {
+	    $('#contact').find('.inline-errors').remove();
+	    if (toggle) {
+	        $("#contact-submit").show();
+	        $('#contact').find('.loader').hide();
+	    }
+	    else {
+	        $("#contact-submit").hide();
+	        $('#contact').find('.loader').show();
+	    }
+	    $("#contact-submit").show();
+	    $('#contact').find('.loader').hide();
+	    $('#contact').find('#comments').val('');
+	}
+	
 });
+
+/** utility methods **/
+function isValidEmail(email) {
+    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return email_regex.test(email.trim());
+}
+
